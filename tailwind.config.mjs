f12e91e1167eaa068acc8b546c7e6e5d728d6936
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-05 13:51:41 +0530

    feat: created clients component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -20,7 +20,22 @@ export default {
 			},
 			colors: {
 				"hhl_blue": "#1873F2"
-			}
+			},
+			animation: {
+				marquee: 'marquee 20s linear infinite',
+				marquee2: 'marquee2 20s linear infinite'
+			},
+
+			keyframes: {
+				marquee: {
+				  	'0%': { transform: 'translateX(0%)' },
+				  	'100%': { transform: 'translateX(-100%)' }
+				},
+				marquee2: {
+				  	'0%': { transform: 'translateX(100%)' },
+				  	'100%': { transform: 'translateX(0%)' }
+				}
+			},
 		},
 	},
 	plugins: [],
